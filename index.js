863e25ccd48598515e97c08053019bc77f62806f
Nadim Mostofa <nadimmostafa334@gmail.com>
2025-04-20 22:04:29 +0600

    added delete api for user

--- index.js
+++ index.js
@@ -188,6 +188,18 @@ async function run() {
     const result = await CollectionOfUsers.find().toArray()
     res.send(result)
   })
+  app.get('/users/:id', async(req,res)=>{
+    const id = req.params.id
+    const query = { _id: new ObjectId(id) }
+    const result = await CollectionOfUsers.findOne(query);
+    res.send(result);
+  })
+  app.delete("/users/:id", async(req,res)=>{
+    const id = req.params.id
+    const query = { _id: new ObjectId(id) }
+    const result = await CollectionOfUsers.deleteOne(query);
+    res.send(result);
+  })
 
 
 
