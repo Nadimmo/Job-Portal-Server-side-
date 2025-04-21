b7774c66cf6176167c3bf43d118b2980e6edf68a
Nadim Mostofa <nadimmostafa334@gmail.com>
2025-04-21 20:03:12 +0600

    added applied jobs some api

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config()
 
 
 app.use(cors())
-app.use(express.json()) 
+app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -25,12 +25,12 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-    const  CollectionOfAllJobs = client.db('job_portalDB').collection('all_jobs')  
-    const  CollectionOfCompany = client.db('job_portalDB').collection('allCompaney')  
-    const  CollectionOfReviews = client.db('job_portalDB').collection('allReviews')  
-    const  CollectionOfLatestBlogs = client.db('job_portalDB').collection('blogs')  
-    const  CollectionOfUsers = client.db('job_portalDB').collection('users')  
-    const  CollectionOfAppliedJobs = client.db('job_portalDB').collection('appliedJobs')  
+  const CollectionOfAllJobs = client.db('job_portalDB').collection('all_jobs')
+  const CollectionOfCompany = client.db('job_portalDB').collection('allCompaney')
+  const CollectionOfReviews = client.db('job_portalDB').collection('allReviews')
+  const CollectionOfLatestBlogs = client.db('job_portalDB').collection('blogs')
+  const CollectionOfUsers = client.db('job_portalDB').collection('users')
+  const CollectionOfAppliedJobs = client.db('job_portalDB').collection('appliedJobs')
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
@@ -44,74 +44,87 @@ async function run() {
       const result = await CollectionOfAllJobs.find().toArray()
       res.send(result)
     })
-    app.get("/allJobs/:id", async(req,res)=>{
-        const Id =  req.params.id
-        const filter = {_id: new ObjectId(Id)}
-        const result = await CollectionOfAllJobs.findOne(filter)
-        res.send(result)
+    app.get("/allJobs/:id", async (req, res) => {
+      const Id = req.params.id
+      const filter = { _id: new ObjectId(Id) }
+      const result = await CollectionOfAllJobs.findOne(filter)
+      res.send(result)
     })
-    app.get("/latestJobs", async(req,res)=>{
-        const result = await CollectionOfAllJobs.find().sort({ _id: -1 }).limit(6).toArray()
-        res.send(result)
+    app.get("/latestJobs", async (req, res) => {
+      const result = await CollectionOfAllJobs.find().sort({ _id: -1 }).limit(6).toArray()
+      res.send(result)
     })
-    app.delete("/allJobs/:id", async(req,res)=>{
-        const Id =  req.params.id
-        const filter = {_id: new ObjectId(Id)}
-        const result = await CollectionOfAllJobs.deleteOne(filter)
-        res.send(result)
-    })           
+    app.delete("/allJobs/:id", async (req, res) => {
+      const Id = req.params.id
+      const filter = { _id: new ObjectId(Id) }
+      const result = await CollectionOfAllJobs.deleteOne(filter)
+      res.send(result)
+    })
     app.get('/updateJob', async (req, res) => {
       const result = await CollectionOfAllJobs.find().toArray()
       res.send(result)
     })
-    app.get("/updateJob/:id", async(req,res)=>{
-        const Id =  req.params.id
-        const filter = {_id: new ObjectId(Id)}
-        const result = await CollectionOfAllJobs.findOne(filter)
-        res.send(result)
+    app.get("/updateJob/:id", async (req, res) => {
+      const Id = req.params.id
+      const filter = { _id: new ObjectId(Id) }
+      const result = await CollectionOfAllJobs.findOne(filter)
+      res.send(result)
     })
-    app.put("/updateJob/:id", async(req,res)=>{
-        const Id =  req.params.id
-        const filter = {_id: new ObjectId(Id)}
-        const updatedJob = req.body
-        const updateDoc = {
-            $set: {
-                title: updatedJob.title,
-                companyName: updatedJob.companyName,
-                location: updatedJob.location,
-                salary: updatedJob.salary,
-                type: updatedJob.type,
-                experienceLevel: updatedJob.experienceLevel,
-                shortDescription: updatedJob.shortDescription,
-                avatar: updatedJob.avatar,
-                responsibilities: updatedJob.responsibilities,
-                requirements: updatedJob.requirements,
-                education: updatedJob.education,
-                deadline: updatedJob.deadline
-            },
-        };
-        const result = await CollectionOfAllJobs.updateOne(filter, updateDoc)
-        res.send(result)
+    app.put("/updateJob/:id", async (req, res) => {
+      const Id = req.params.id
+      const filter = { _id: new ObjectId(Id) }
+      const updatedJob = req.body
+      const updateDoc = {
+        $set: {
+          title: updatedJob.title,
+          companyName: updatedJob.companyName,
+          location: updatedJob.location,
+          salary: updatedJob.salary,
+          type: updatedJob.type,
+          experienceLevel: updatedJob.experienceLevel,
+          shortDescription: updatedJob.shortDescription,
+          avatar: updatedJob.avatar,
+          responsibilities: updatedJob.responsibilities,
+          requirements: updatedJob.requirements,
+          education: updatedJob.education,
+          deadline: updatedJob.deadline
+        },
+      };
+      const result = await CollectionOfAllJobs.updateOne(filter, updateDoc)
+      res.send(result)
     })
 
 
     // ...............user apply job api colloection..........
-    app.post("/appliedJobs", async(req,res) =>{
+    app.post("/appliedJobs", async (req, res) => {
       const jobs = req.body;
       const result = await CollectionOfAppliedJobs.insertOne(jobs)
       res.send(result)
     })
-    app.get("/appliedJobs", async(req,res)=>{
+    // ....show all applied jobs.....
+    app.get("/appliedAllJobs", async (req, res) => {
       const result = await CollectionOfAppliedJobs.find().toArray()
       res.send(result)
     })
-    //get apply jobs by user email
-    // app.get("/appliedJobs", async(req,res)=>{
-    //   const email = req.query.email
-    //   const query = {email: email}
-    //   const result = await CollectionOfAppliedJobs.find(query).toArray()
-    //   res.send(result)
-    // })
+    //show applied jobs by user email
+    app.get("/appliedJobs", async (req, res) => {
+      const email = req.query.email
+      const query = { email: email }
+      const result = await CollectionOfAppliedJobs.find(query).toArray()
+      res.send(result)
+    })
+    app.get("/appliedJobs/:id", async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await CollectionOfAppliedJobs.findOne(query);
+      res.send(result);
+    })
+    app.delete("/appliedJobs/:id", async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await CollectionOfAppliedJobs.deleteOne(query);
+      res.send(result);
+    })
 
 
     // ..............all  company collection api................./
@@ -124,11 +137,11 @@ async function run() {
       const result = await CollectionOfCompany.find().toArray()
       res.send(result)
     })
-    app.get("/allCompanies/:id", async(req,res)=>{
-        const id = req.params.id
-        const query = { _id: new ObjectId(id) }
-        const result = await CollectionOfCompany.findOne(query);
-        res.send(result);
+    app.get("/allCompanies/:id", async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await CollectionOfCompany.findOne(query);
+      res.send(result);
     })
 
 
@@ -137,92 +150,92 @@ async function run() {
       const result = await CollectionOfReviews.find().toArray()
       res.send(result)
     })
-    app.get("/allReviews/:id", async(req,res)=>{
+    app.get("/allReviews/:id", async (req, res) => {
       const id = req.params.id
       const query = { _id: new ObjectId(id) }
       const result = await CollectionOfReviews.findOne(query);
-      res.send(result); 
-  })
+      res.send(result);
+    })
 
 
-  // ..........blogs and news api...........
-  app.post('/latestBlogs', async (req, res) => {
-    const newBlog = req.body;
-    const result = await CollectionOfLatestBlogs.insertOne(newBlog);
-    res.send(result)
-  })
-  app.get('/latestBlogs', async (req, res) => {
-    const result = await CollectionOfLatestBlogs.find().toArray()
-    res.send(result)
-  })
-  app.get("/latestBlogs/:id", async(req,res)=>{
+    // ..........blogs and news api...........
+    app.post('/latestBlogs', async (req, res) => {
+      const newBlog = req.body;
+      const result = await CollectionOfLatestBlogs.insertOne(newBlog);
+      res.send(result)
+    })
+    app.get('/latestBlogs', async (req, res) => {
+      const result = await CollectionOfLatestBlogs.find().toArray()
+      res.send(result)
+    })
+    app.get("/latestBlogs/:id", async (req, res) => {
       const id = req.params.id
       const query = { _id: new ObjectId(id) }
       const result = await CollectionOfLatestBlogs.findOne(query);
       res.send(result);
-  })
-  app.delete("/latestBlogs/:id", async(req,res)=>{
+    })
+    app.delete("/latestBlogs/:id", async (req, res) => {
       const id = req.params.id
       const query = { _id: new ObjectId(id) }
       const result = await CollectionOfLatestBlogs.deleteOne(query);
       res.send(result);
-  })
+    })
 
-  app.get('/updateBlog', async (req, res) => {
-    const result = await CollectionOfLatestBlogs.find().toArray()
-    res.send(result)
-  })
-  app.get("/updateBlog/:id", async(req,res)=>{
-      const Id =  req.params.id
-      const filter = {_id: new ObjectId(Id)}
+    app.get('/updateBlog', async (req, res) => {
+      const result = await CollectionOfLatestBlogs.find().toArray()
+      res.send(result)
+    })
+    app.get("/updateBlog/:id", async (req, res) => {
+      const Id = req.params.id
+      const filter = { _id: new ObjectId(Id) }
       const result = await CollectionOfLatestBlogs.findOne(filter)
       res.send(result)
-  })
-  app.put("/updateBlog/:id", async(req,res)=>{
-      const Id =  req.params.id
-      const filter = {_id: new ObjectId(Id)}
+    })
+    app.put("/updateBlog/:id", async (req, res) => {
+      const Id = req.params.id
+      const filter = { _id: new ObjectId(Id) }
       const updatedJob = req.body
       const updateDoc = {
-          $set: {
-              title: updatedJob.title,
-              image: updatedJob.image,
-              summary: updatedJob.summary,
-              category: updatedJob.category,
-              date: updatedJob.date,
-          },
+        $set: {
+          title: updatedJob.title,
+          image: updatedJob.image,
+          summary: updatedJob.summary,
+          category: updatedJob.category,
+          date: updatedJob.date,
+        },
       };
       const result = await CollectionOfLatestBlogs.updateOne(filter, updateDoc)
       res.send(result)
-  })
+    })
 
-  // ................user collection api...........
-  app.post('/users', async (req, res) => {
-    const newUser = req.body;
-    const email = req.body.email
-    const filter = { email: email }
-    const existing = await CollectionOfUsers.findOne(filter)
-    if (existing) {
-      return res.send({ message: 'User already exists' })
-    }
-    const result = await CollectionOfUsers.insertOne(newUser);
-    res.send(result)
-  })
-  app.get('/users', async (req, res) => {
-    const result = await CollectionOfUsers.find().toArray()
-    res.send(result)
-  })
-  app.get('/users/:id', async(req,res)=>{
-    const id = req.params.id
-    const query = { _id: new ObjectId(id) }
-    const result = await CollectionOfUsers.findOne(query);
-    res.send(result);
-  })
-  app.delete("/users/:id", async(req,res)=>{
-    const id = req.params.id
-    const query = { _id: new ObjectId(id) }
-    const result = await CollectionOfUsers.deleteOne(query);
-    res.send(result);
-  })
+    // ................user collection api...........
+    app.post('/users', async (req, res) => {
+      const newUser = req.body;
+      const email = req.body.email
+      const filter = { email: email }
+      const existing = await CollectionOfUsers.findOne(filter)
+      if (existing) {
+        return res.send({ message: 'User already exists' })
+      }
+      const result = await CollectionOfUsers.insertOne(newUser);
+      res.send(result)
+    })
+    app.get('/users', async (req, res) => {
+      const result = await CollectionOfUsers.find().toArray()
+      res.send(result)
+    })
+    app.get('/users/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await CollectionOfUsers.findOne(query);
+      res.send(result);
+    })
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await CollectionOfUsers.deleteOne(query);
+      res.send(result);
+    })
 
 
 
